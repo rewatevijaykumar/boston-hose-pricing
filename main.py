83536e0a51bed6fcff48a0187293dfd68683e3af
rewatevijaykumar <rewatevijaykumar@gmail.com>
2022-09-30 17:09:29 +0530

    web app ready

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+from ast import Try
 from flask import Flask, request, app, jsonify, url_for, render_template
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
@@ -22,6 +22,18 @@ def predict_api():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = [float(x) for x in request.form.values()]
+        new_data = scaler.transform(np.array(data).reshape(1,-1))
+        print(new_data)
+        output = model.predict(new_data)[0]
+    except ValueError:
+            return render_template('home.html',error = "Please enter valid values")
+    return render_template('home.html',prediction=round(output,2))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
